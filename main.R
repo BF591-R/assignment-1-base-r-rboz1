a8e219d0d2b88c974e4852484be78935806a55d3
rachel Bozadjian <rvboz@scc-wi2.scc.bu.edu>
2024-09-15 20:43:20 -0400

    fixed less_than_zero

--- main.R
+++ main.R
@@ -1,20 +1,13 @@
-library(tibble)
-library(dplyr)
-library(tidyr)
-library(readr)
-library(stringr)
-# library(purrr)
-
 # ----------------------- Helper Functions to Implement ------------------------
 
-#' Evaluate whether the argument is less than 2
+#' Evaluate whether the argument is less than 0
 #'
 #' Returns TRUE if the numeric argument x is a prime number, otherwise returns
 #' FALSE
 #'
 #' @param x (numeric): the numeric value(s) to test
 #'
-#' @return logical value or vector indicating whether the numeric argument is less than 2
+#' @return logical value or vector indicating whether the numeric argument is less than 0
 #' @export
 #'
 #' @examples
@@ -25,7 +18,7 @@ library(stringr)
 #' less_than_zero(c(-1,0,1,2,3,4))
 #' [1] TRUE FALSE FALSE FALSE FALSE FALSE
 less_than_zero <- function(x) {
-  return(NULL)
+  return(x<0)
 }
 
 #' Evaluate whether the argument is between two numbers
@@ -155,144 +148,6 @@ summarize_matrix <- function(x, na.rm=FALSE) {
   return(NULL)
 }
 
-#' Read the expression data "csv" file.
-#'
-#' Function to read microarray expression data stored in a csv file. The
-#' function should return a sample x gene tibble, with an extra column named
-#' "subject_id" that contains the geo accession ids for each subject.
-#'
-#' @param filename (str): the file to read.
-#'
-#' @return ex_int_data (tibble)
-#' @export
-#'
-#' @examples expr_mat <- read_expression_table('example_intensity_data.csv')
-read_expression_table <- function(filename) {
-  #import and transpose data
-  ex_int_data <- read_delim(filename, delim = " ", col_names = FALSE) %>%
-    as_data_frame() %>%
-    t() %>%
-    as_tibble()
-  
-  #rename columns with first row of data
-  colnames(ex_int_data) <- ex_int_data[1,]
-  
-  #remove first row and rename "probe" column to "subject_id"
-  ex_int_data <- ex_int_data[-1,] %>%
-    dplyr::rename("subject_id" = "probe")
-  
-  return(ex_int_data)
-}
-
-#' Replaces all '.' in a string with '_'
-#'
-#' @param str String to operate upon.
-#'
-#' @return reformatted string.
-#' @export
-#'
-#' @examples
-#' period_to_underscore("foo.bar")
-#' "foo_bar"
-period_to_underscore <- function(str) {
-  return (str_replace_all(str, fixed("."), "_"))
-}
-
-# rename variables:
-# Age_at_diagnosis to Age
-# SixSubtypesClassification to Subtype
-# normalizationcombatbatch to Batch
-
-#' Rename and select specified columns.
-#'
-#' Function to rename Age_at_diagnosis, SixSubtypesClassification, and
-#' normalizationcombatbatch columns to Age, Subtype, and Batch, respectively. A
-#' subset of the data should be returned, containing only the Sex, Age, TNM_Stage,
-#' Tumor_Location, geo_accession, KRAS_Mutation, Subtype, and Batch columns.
-#'
-#' @param data (tibble) metadata information for each sample
-#'
-#' @return (tibble) renamed and subsetted tibble
-#' @export
-#'
-#' @examples rename_and_select(metadata)
-#' 
-#' 
-rename_and_select <- function(data) {
-  return (NULL)
-}
-
-#' Create new "Stage" column containing "stage " prefix.
-#'
-#' Creates a new column "Stage" with elements following a "stage x" format, where
-#' `x` is the cancer stage data held in the existing TNM_Stage column. Stage
-#' should have a factor data type.
-#'
-#' @param data  (tibble) metadata information for each sample
-#'
-#' @return (tibble) updated metadata with "Stage" column
-#' @export
-#'
-#' @examples metadata <- stage_as_factor(metadata)
-stage_as_factor <- function(data) {
-  return (NULL)
-}
-
-#' Calculate age of samples from a specified sex.
-#'
-#' @param data (tibble) metadata information for each sample
-#' @param sex (str) which sex to calculate mean age. Possible values are "M"
-#' and "F"
-#'
-#' @return (float) mean age of specified samples
-#' @export
-#'
-#' @examples mean_age_by_sex(metadata, "F")
-mean_age_by_sex <- function(data, sex) {
-  return (NULL)
-}
-
-
-#' Calculate average age of samples within each cancer stage. Stages should be
-#' from the newly created "Stage" column.
-#'
-#' @param data (tibble) metadata information for each sample
-#'
-#' @return (tibble) summarized tibble containing average age for all samples from
-#' each stage.
-#' @export
-#'
-#' @examples age_by_stage(data)
-age_by_stage <- function(data) {
-  return (NULL)
-}
-
-#' Create a cross tabulated table for Subtype and Stage using dplyr methods.
-#'
-#' @param data (tibble) metadata information for each sample
-#'
-#' @return (tibble) table where rows are the cancer stage of each sample, and the
-#' columns are each cancer subtype. Elements represent the number of samples from
-#' the corresponding stage and subtype. If no instances of a specific pair are
-#' observed, a zero entry is expected.
-#' @export
-#'
-#' @examples cross_tab <- dplyr_cross_tab(metadata)
-subtype_stage_cross_tab <- function(data) {
-  return (NULL)
-}
-
-#' Summarize average expression and probe variability over expression matrix.
-#'
-#' @param exprs An (n x p) expression matrix, where n is the number of samples,
-#' and p is the number of probes.
-#'
-#' @return A summarized tibble containing `main_exp`, `variance`, and `probe`
-#' columns documenting average expression, probe variability, and probe ids,
-#' respectively.
-summarize_expression <- function(exprs) {
-  return (NULL)
-}
 # ------------ Helper Functions Used By Assignment, You May Ignore ------------
 sample_normal <- function(n, mean=0, sd=1) {
   set.seed(1337)
