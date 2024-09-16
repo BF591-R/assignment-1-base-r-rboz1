08f5728573448618db16c2c49cce2bf273e815b8
rachel Bozadjian <rvboz@scc-wi2.scc.bu.edu>
2024-09-15 20:10:06 -0400

    fixed up through function period_to_underscore

--- main.R
+++ main.R
@@ -1,166 +1,150 @@
+library(tibble)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(stringr)
+# library(purrr)
+
+
 # ----------------------- Helper Functions to Implement ------------------------
 
-#' Evaluate whether the argument is less than 2
+#' Read the expression data "csv" file.
 #'
-#' Returns TRUE if the numeric argument x is a prime number, otherwise returns
-#' FALSE
+#' Function to read microarray expression data stored in a csv file. The
+#' function should return a sample x gene tibble, with an extra column named
+#' "subject_id" that contains the geo accession ids for each subject.
 #'
-#' @param x (numeric): the numeric value(s) to test
+#' @param filename (str): the file to read.
 #'
-#' @return logical value or vector indicating whether the numeric argument is less than 2
+#' @return ex_int_data (tibble)
 #' @export
 #'
-#' @examples
-#' less_than_zero(-1)
-#' [1] TRUE
-#' less_than_zero(10)
-#' [1] FALSE
-#' less_than_zero(c(-1,0,1,2,3,4))
-#' [1] TRUE FALSE FALSE FALSE FALSE FALSE
-less_than_zero <- function(x) {
-    return(NULL)
+#' @examples expr_mat <- read_expression_table('example_intensity_data.csv')
+read_expression_table <- function(filename) {
+  #import and transpose data
+  ex_int_data <- read_delim(filename, delim = " ", col_names = FALSE) %>%
+    as_data_frame() %>%
+    t() %>%
+    as_tibble()
+  
+  #rename columns with first row of data
+  colnames(ex_int_data) <- ex_int_data[1,]
+  
+  #remove first row and rename "probe" column to "subject_id"
+  ex_int_data <- ex_int_data[-1,] %>%
+    dplyr::rename("subject_id" = "probe")
+  
+  return(ex_int_data)
 }
 
-#' Evaluate whether the argument is between two numbers
+#' Replaces all '.' in a string with '_'
 #'
-#' Returns TRUE if the numeric argument x is contained within the open interval
-#' (a, b), otherwise return FALSE.
+#' @param str String to operate upon.
 #'
-#' @param x (numeric): the numeric value(s) to test
-#' @param a (number): the lower bound
-#' @param b (number): the upper bound
-#'
-#' @return logical value of same type as input (e.g. scalar, vector, or matrix)
+#' @return reformatted string.
 #' @export
 #'
 #' @examples
-#' is_between(3,1,5)
-#' [1] TRUE
-#' is_between(c(1,9,5,2), 1, 5)
-#' [1] FALSE FALSE FALSE TRUE
-#' is_between(matrix(1:9, nrow=3, byrow=TRUE), 1, 5)
-#'       [,1]  [,2]  [,3]
-#' [1,] FALSE  TRUE  TRUE
-#' [2,]  TRUE FALSE FALSE
-#' [3,] FALSE FALSE FALSE
-is_between <- function(x, a, b) {
-    return(NULL)
+#' period_to_underscore("foo.bar")
+#' "foo_bar"
+period_to_underscore <- function(str) {
+  return (str_replace_all(str, fixed("."), "_"))
 }
 
-#' Return the values of the input vector that are not NA
-#'
-#' Returns the values of the input vector `x` that are not NA
-#'
-#' @param x (numeric): numeric vector to remove NA from 
-#'
-#' @return numeric vector `x` with all `NA` removed
-#' @export
-#'
-#' @examples
-#' x <- c(1,2,NA,3)
-#' rm_na(x)
-#' [1] 1 2 3
-rm_na <- function(x) {
-    return(NULL)
-}
+# rename variables:
+# Age_at_diagnosis to Age
+# SixSubtypesClassification to Subtype
+# normalizationcombatbatch to Batch
 
-#' Calculate the median of each row of a matrix
+#' Rename and select specified columns.
 #'
-#' Given the matrix x with n rows and m columns, return a numeric vector of
-#' length n that contains the median value of each row of x
+#' Function to rename Age_at_diagnosis, SixSubtypesClassification, and
+#' normalizationcombatbatch columns to Age, Subtype, and Batch, respectively. A
+#' subset of the data should be returned, containing only the Sex, Age, TNM_Stage,
+#' Tumor_Location, geo_accession, KRAS_Mutation, Subtype, and Batch columns.
 #'
-#' @param x (numeric matrix): matrix to compute median along rows
+#' @param data (tibble) metadata information for each sample
 #'
-#' @return (numeric vector) vector containing median row values
+#' @return (tibble) renamed and subsetted tibble
 #' @export
 #'
-#' @examples
-#' m <- matrix(1:9, nrow=3, byrow=T)
-#' row_medians(m)
-#' [1] 1 4 7
+#' @examples rename_and_select(metadata)
 #' 
-row_medians <- function(x) {
-    return(NULL)
-}
-
-#' Evaluate each row of a matrix with a provided function
-#'
-#' Given the matrix `x` with n rows and m columns, return a numeric vector of
-#' length n that contains the returned values from the evaluation of the
-#' function `fn` on each row. `fn` should be a function that accepts a vector as
-#' input and returns a scalar value
-#'
-#' @param x (numeric matrix): matrix to evaluate the function along rows
-#' @param fn (function) function that accepts a vector as input and returns a scalar
-#' @param na.rm (logical) OPTIONAL: a logical evaluating to `TRUE` or `FALSE`
-#'   indicating whether `NA` values should be stripped before computing summary
-#'
-#' @return (numeric vector) vector of the evaluation of `fn` on rows of `x`
-#' @export
-#'
-#' @examples
-#' m <- matrix(1:9, nrow=3, byrow=T)
-#' summarize_rows(m, min)
-#' [1] 1 4 7
-#' summarize_rows(m, mean)
-#' [1] 2 5 8
-summarize_rows <- function(x, fn, na.rm=FALSE) {
-    return(NULL)
-}
-
-#' Summarize matrix rows into data frame
-#'
-#' Summarizes the rows of matrix `x`. Returns a data frame with the following
-#' columns in order and the corresponding value for each row:
 #' 
-#'   * mean - arithmetic mean
-#'   * stdev - standard deviation
-#'   * median - median
-#'   * min - minimum value
-#'   * max - maximum value
-#'   * num_lt_0 - the number of values less than 0
-#'   * num_btw_1_and_5 - the number of values between 1 and 5
-#'   * num_na - the number of missing (NA) values (ignoring value of na.rm)
+rename_and_select <- function(data) {
+  return (NULL)
+}
+
+
+#' Create new "Stage" column containing "stage " prefix.
 #'
-#' @param x (numeric matrix): matrix to evaluate the function along rows
-#' @param na.rm (logical) OPTIONAL: a logical evaluating to `TRUE` or `FALSE`
-#'   indicating whether `NA` values should be stripped before computing summary
+#' Creates a new column "Stage" with elements following a "stage x" format, where
+#' `x` is the cancer stage data held in the existing TNM_Stage column. Stage
+#' should have a factor data type.
 #'
-#' @return (data frame) data frame containing summarized values for each row of `x`
+#' @param data  (tibble) metadata information for each sample
+#'
+#' @return (tibble) updated metadata with "Stage" column
 #' @export
 #'
-#' @examples
-#' m <- matrix(1:9, nrow=3, byrow=T)
-#' summarize_matrix(m)
-#'   mean stdev median min max num_lt_0 num_btw_1_and_5 num_na
-#' 1    2     1      2   1   3        0               3      0
-#' 2    5     1      5   4   6        0               1      0
-#' 3    8     1      8   7   9        0               0      0
+#' @examples metadata <- stage_as_factor(metadata)
+stage_as_factor <- function(data) {
+  return (NULL)
+}
+
+
+#' Calculate age of samples from a specified sex.
 #'
-#' m <- matrix(rnorm(1000), nrow=4, byrow=T)
-#' summarize_matrix(m)
-#'          mean    stdev      median       min      max num_lt_0 num_btw_1_and_5 num_na
-#' 1 -0.02220010 1.006901  0.02804177 -3.485147 3.221089      120              61      0
-#' 2 -0.01574033 1.026951 -0.04725656 -2.967057 2.571608      112              70      0
-#' 3 -0.09040182 1.027559 -0.02774705 -3.026888 2.353087      130              54      0
-#' 4  0.09518138 1.030461  0.11294781 -3.409049 2.544992       90              72      0
-summarize_matrix <- function(x, na.rm=FALSE) {
-    return(NULL)
+#' @param data (tibble) metadata information for each sample
+#' @param sex (str) which sex to calculate mean age. Possible values are "M"
+#' and "F"
+#'
+#' @return (float) mean age of specified samples
+#' @export
+#'
+#' @examples mean_age_by_sex(metadata, "F")
+mean_age_by_sex <- function(data, sex) {
+  return (NULL)
 }
 
-# ------------ Helper Functions Used By Assignment, You May Ignore ------------
-sample_normal <- function(n, mean=0, sd=1) {
-    return(NULL)
+
+#' Calculate average age of samples within each cancer stage. Stages should be
+#' from the newly created "Stage" column.
+#'
+#' @param data (tibble) metadata information for each sample
+#'
+#' @return (tibble) summarized tibble containing average age for all samples from
+#' each stage.
+#' @export
+#'
+#' @examples age_by_stage(data)
+age_by_stage <- function(data) {
+  return (NULL)
 }
 
-sample_normal_w_missing <- function(n, mean=0, sd=1, missing_frac=0.1) {
-    return(NULL)
+#' Create a cross tabulated table for Subtype and Stage using dplyr methods.
+#'
+#' @param data (tibble) metadata information for each sample
+#'
+#' @return (tibble) table where rows are the cancer stage of each sample, and the
+#' columns are each cancer subtype. Elements represent the number of samples from
+#' the corresponding stage and subtype. If no instances of a specific pair are
+#' observed, a zero entry is expected.
+#' @export
+#'
+#' @examples cross_tab <- dplyr_cross_tab(metadata)
+subtype_stage_cross_tab <- function(data) {
+  return (NULL)
 }
 
-simulate_gene_expression <- function(num_samples, num_genes) {
-    return(NULL)
-}
-
-simulate_gene_expression_w_missing <- function(num_samples, num_genes, missing_frac=0.1) {
-    return(NULL)
+#' Summarize average expression and probe variability over expression matrix.
+#'
+#' @param exprs An (n x p) expression matrix, where n is the number of samples,
+#' and p is the number of probes.
+#'
+#' @return A summarized tibble containing `main_exp`, `variance`, and `probe`
+#' columns documenting average expression, probe variability, and probe ids,
+#' respectively.
+summarize_expression <- function(exprs) {
+  return (NULL)
 }
